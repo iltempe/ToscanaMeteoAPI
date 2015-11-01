29b4f4608ff6a0e4db1412554a68010de0845a2f
iltempe <pratosmart@gmail.com>
2015-11-01 22:41:04 +0100

    add risk data

--- api.php
+++ api.php
@@ -3,17 +3,19 @@
 include "xml2json.php";
 
 //nome comune (for debug)
-$comune="Firenze";
+$comune="Vaiano";
+
+get_rischio($comune);
 
 /**
  * Genera i dati JSON dato un nome di un comune per cui sono previsti i dati
  * data source http://dati.toscana.it/
  * @param comune il nome del comune di cui si vuol sapere il meteo
- * @return $json data
+ * @return $json data (null if data are not available)
  * @author Matteo Tempestini 
  */
  
-function get_meteo_comune($comune)
+function get_meteo($comune)
 {
 	//lista comuni disponibili
 	$meteo_list_url="http://www.lamma.rete.toscana.it/previ/ita/xml/lista_comuni.xml";
@@ -32,6 +34,59 @@ function get_meteo_comune($comune)
 	$xmlNode = simplexml_load_file($url_meteo);
 	$arrayData = xmlToArray($xmlNode);
 	$json=json_encode($arrayData);
+	return $json;
+}
+
+/**
+ * Genera i dati JSON dato un nome di un comune per cui sono previsti i dati
+ * data source http://www.sir.toscana.it/
+ * @param comune il nome del comune di cui si vuol sapere il rischio
+ * @return $json data (null if data are not available)
+ * - rischi oggi
+ * --idrogeologico
+ * --idraulico
+ * --vento
+ * --mareggiate
+ * --neve
+ * --ghiaccio
+ * --temporali
+ * - rischi domani
+ * --idrogeologico
+ * --idraulico
+ * --vento
+ * --mareggiate
+ * --neve
+ * --ghiaccio
+ * --temporali
+ * @author Matteo Tempestini 
+ */
+ 
+function get_rischio($comune)
+{
+	date_default_timezone_set('UTC');
+	$today = date("Ymd");   
+
+	//Gestione Rischio Centro Funzionale Regione Toscana
+	$xmlrisk=simplexml_load_file("http://www.sir.toscana.it/supports/xml/risks_395/".$today.".xml"); 
+	if ($xmlrisk==false)
+	{
+		print("Errore nella ricerca del file relativo al rischio");
+		$json=null;
+	}else
+	{
+
+		//cerca il nodo dell'area di rischio
+		$node = $xmlrisk->xpath("//area[contains(comuni,'$comune')]");
+		$node=$node[0];
+		$area=$node[@name];
+		
+		//$tags = $xmlrisk->xpath("rischio[@name]");
+		$rischi = $xmlrisk->xpath("//rischi/rischio/area[contains(@name,'$area')]");
+		print_r($tags);
+		//$arrayData = xmlToArray($rischi);
+		$json=json_encode($rischi);
+		print_r($json);
+	}
 	return $json;
 }
 
